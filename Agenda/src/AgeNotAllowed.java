bdd86caaf4986f7f42f4c7cd22e7e417d4cfbe6b
Israel <israeloliveiraudi@gmail.com>
2026-05-19 11:05:33 -0300

    Delete Agenda directory

--- Agenda/src/AgeNotAllowed.java
+++
@@ -1,9 +0,0 @@
-public class AgeNotAllowed extends RuntimeException {
-    public AgeNotAllowed(String message) {
-        super(message);
-    }
-
-    public AgeNotAllowed(){
-        System.out.println("A pessoa precisa ser maior de 18 anos");
-    }
-}
--- Agenda/src/method.java
+++
@@ -1,5 +0,0 @@
-public interface method {
-    public abstract void add(Contatos p);
-    public abstract void remover(String numero);
-    public abstract void verificar(String numero);
-}
